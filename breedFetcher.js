71ccbaa5f21a974e75c57d7fc94c5c446be5bcd4
Joyce Ann Miguel <joyce.a.miguel@gmail.com>
2023-05-14 09:54:52 -0400

    refractored

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,11 +2,15 @@ const request = require('request');
 const catName = process.argv[2];
 const web = "https://api.thecatapi.com/v1/breeds/search?q=" + catName;
 
-request(web, (error, response, body) => {
-  const data = JSON.parse(body);
-  const description = data[0].description;
-  console.log(description);
-});
+const fetchBreedDescription = function(breedName, callback) {
+  request(web, (error, response, body) => {
+    const data = JSON.parse(body);
+    breedName = data[0].description;
+    callback(breedName);
+  });
+};
+
+module.exports = { fetchBreedDescription };
 
 // console.log('error:', error); // Print the error if one occurred
 //   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
