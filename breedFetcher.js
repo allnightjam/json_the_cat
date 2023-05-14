7cebe4085d25f1e9be582544b8a98e6eaf776847
Joyce Ann Miguel <joyce.a.miguel@gmail.com>
2023-05-14 09:35:58 -0400

    adding breed fetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,14 @@
+const request = require('request');
+const catName = process.argv[2];
+const web = "https://api.thecatapi.com/v1/breeds/search?q=" + catName;
+
+request(web, (error, response, body) => {
+  const data = JSON.parse(body);
+  const description = data[0].description;
+  console.log(description);
+});
+
+// console.log('error:', error); // Print the error if one occurred
+//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//   console.log('body:', body); // Print the HTML for the Google homepage.
 
